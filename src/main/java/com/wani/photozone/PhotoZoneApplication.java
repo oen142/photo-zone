20bcd8ec041aeeb5f7ee7431717edf994b1f59ad
wani <oen142@naver.com>
2021-04-01 16:29:10 +0900

    feat : first init

---
+++ src/main/java/com/wani/photozone/PhotoZoneApplication.java
@@ -0,0 +1,13 @@
+package com.wani.photozone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/photozone/PhotoZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.photozone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
