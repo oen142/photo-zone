b797881bce2b0c6db8e24bebd453394e38a122fa
Wani <46095454+oen142@users.noreply.github.com>
2021-04-01 16:38:00 +0900

    feat : 멀티 모듈형 프로젝트 완성 (#2)

---
+++ application-api/src/test/java/com/wani/applicationapi/ApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.applicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
